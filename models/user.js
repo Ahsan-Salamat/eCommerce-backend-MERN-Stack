96649e1a484a05997b28830f34da7c5a6636af52
MrHacker10100 <ahsansalamat10100@gmail.com>
2025-07-02 22:27:56 +0500

    user model

---
+++ models/user.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
+        maxlength: [50, "Name cannot exceed 50 characters"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password"],
+        minlength: [6, "Password should be longer than 6 characters"],
+        select: false 
+    }
+})
